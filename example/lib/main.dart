78c4be508081769a46050820963ecc191ac20931
Dimas Nur Alif R <dimasnur.dev@gmail.com>
2022-12-29 05:04:15 +0700

    ✨ feat: update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,5 +2,5 @@ import 'package:terbilang_id/terbilang_id.dart';
 
 void main() {
   const number = 900123.0;
-  Terbilang().terbilang(number); // Sembilan Ratus Ribu Seratus Dua Puluh Tiga
+  Terbilang.format(number); // Sembilan Ratus Ribu Seratus Dua Puluh Tiga
 }
